b0052c94420d267e288ef5aa052e3678d55d9ce8
Chris Atkinson <christopher.k.atkinson@gmail.com>
2019-12-05 10:33:49 -0600

    All four opcodes are working correctly with paramaters! How cool!

--- day5/src/Lib.hs
+++ day5/src/Lib.hs
@@ -5,6 +5,7 @@ module Lib
 
 import Data.Char (digitToInt)
 import Data.List (reverse)
+import Data.Maybe
 import Data.List.Split (splitOneOf)
 import Data.Sequence (Seq)
 import qualified Data.Sequence as S
@@ -13,11 +14,13 @@ getInput :: FilePath -> IO (Seq Int)
 getInput path = do contents <- readFile path
                    return (S.fromList $ map read $ splitOneOf ", " contents)
 
-data OpCode = ADD | MUL | IN | OUT deriving (Show, Eq)
+data OpCode  = ADD | MUL | IN | OUT | HLT deriving (Show, Eq)
 data ParMode = POS | IMM deriving (Show, Eq)
 type Pointer = Int
-type OpInst = Int -- Think something like 01002, encoding opcode and parmode
-type Arg = Int
+type OpInst  = Int -- Think something like 01002, encoding opcode and parmode
+type ParArg  = (Int, ParMode)
+type Arg     = Int
+type Memory  = Seq Int
 
 movePointer :: OpCode -> Pointer -> Pointer
 movePointer oc ptr = let shift = numParams oc + 1
@@ -27,12 +30,13 @@ movePointer oc ptr = let shift = numParams oc + 1
                           IN  -> ptr + shift
                           OUT -> ptr + shift
 
-opcode :: OpInst -> OpCode
-opcode ins = case ins `mod` 10 of
-               1 -> ADD
-               2 -> MUL
-               3 -> IN
-               4 -> OUT
+opCode :: OpInst -> OpCode
+opCode ins = case ins `mod` 100 of
+               1  -> ADD
+               2  -> MUL
+               3  -> IN
+               4  -> OUT
+               99 -> HLT
 
 numParams :: OpCode -> Int
 numParams oc = case oc of
@@ -40,6 +44,7 @@ numParams oc = case oc of
                  MUL -> 3
                  IN  -> 1
                  OUT -> 1
+                 HLT -> 0
 
 parMode :: Int -> ParMode
 parMode 0 = POS
@@ -49,49 +54,59 @@ parMode 1 = IMM
 -- together with the list of arguments so that the extra 0's at the end get
 -- dropped.
 parModes :: OpInst -> [ParMode]
-parModes ins = map (parMode . digitToInt)  
+parModes ins = take (numParams $ opCode ins) $
+               map (parMode . digitToInt)  
                    (drop 2 $ reverse (show ins)) ++ replicate 3 POS
 
--- -- -- -- -- -- 
+getArg :: Memory -> ParArg -> Arg
+getArg mem (arg, par) = case par of
+                          POS -> S.index mem arg
+                          IMM -> arg
 
-execute :: Seq Int -> Int -> Seq Int
-execute s pos
-  | S.index s pos == 99 = s 
-  | S.index s pos == 1  = execute (S.adjust'
-                                  (const $ S.index s si1 + 
-                                           S.index s si2)
-                                  si3
-                                  s)
-                                  (pos + 4) -- add
-  | S.index s pos == 2  = execute (S.adjust'
-                                  (const $ S.index s si1 * 
-                                           S.index s si2)
-                                  si3
-                                  s)
-                                  (pos + 4) -- multiply
-  | otherwise           = error "Operation is undefined"
-  where si1 = S.index s (pos + 1)
-        si2 = S.index s (pos + 2)
-        si3 = S.index s (pos + 3)
+memLookup :: Memory -> Pointer -> Int
+memLookup mem ptr = fromMaybe 0 (S.lookup ptr mem)
 
--- before running the program, replace position 1 with the value 12 and replace
--- position 2 with the value 2
+readInt :: IO Int
+readInt = read <$> getLine
 
-answer1 :: Seq Int -> Int
-answer1 s = S.index (execute s' 0) 0
-  where s' = S.update 2 2 $ S.update 1 12 s
+execute :: Memory -> Pointer -> IO Memory
+execute mem ptr
+  | currentoc == HLT = return mem
+  | currentoc == ADD = execute (S.adjust'
+                                  (const $ head args + (args !! 1))
+                                  (args !! 2)
+                                  mem)
+                                  shift
+  | currentoc == MUL = execute (S.adjust'
+                                  (const $ head args * (args !! 1))
+                                  (args !! 2)
+                                  mem)
+                                  shift
+  | currentoc == IN  = do x <- readInt
+                          execute (S.adjust' (const x)
+                                             (head args)
+                                             mem)
+                                  shift
+  | currentoc == OUT = do print $ head args
+                          execute mem shift
+  where ptrStar       = memLookup mem ptr
+        currentoc     = opCode ptrStar
+        numArgs       = numParams currentoc
+        currentParams = parModes ptrStar
+        currentPArgs  = zip [ memLookup mem (ptr + d) | d <- [1 .. numArgs] ] currentParams
+        args          = map (getArg mem) currentPArgs
+        shift = movePointer currentoc ptr
+
+mem :: Memory
+mem = S.fromList [11101,1,1,1,99]
+
+mem1 :: Memory
+mem1 = S.fromList [3,0,11101,1,1,1,99]
+
+mem2 :: Memory
+mem2 = S.fromList [4,2,99]
 
 
--- part 2
--- determine what pair of inputs produces the output 19690720
---
-
-output :: Seq Int -> Int -> Int -> Int
-output s noun verb = S.index (execute s' 0) 0
-  where s' = S.update 2 verb $ S.update 1 noun s
-
-formatOutput :: [(Int,Int)] -> Int
-formatOutput ls = 100 * fst (head ls) * snd (head ls)
 
 
 five :: IO ()
