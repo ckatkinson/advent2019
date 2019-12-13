634874e6443c256e4055019a20b51d3df5337fa7
Chris Atkinson <christopher.k.atkinson@gmail.com>
2019-12-13 10:50:21 -0600

    Really struggling to get this running. I think I figured it out though. My implementation of IntCode has a single buffer: Input reads from the front of it and output ++'s to the back. I think that what is happening (including with the weird fixBoard hack) is that this program is reading some outputs as inputs! I'll need to fix this.

--- day13/src/Lib.hs
+++ day13/src/Lib.hs
@@ -3,8 +3,10 @@ module Lib
     ) where
 
 import IntCode
+import Data.List.Split (chunksOf)
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as M
+import qualified Data.Sequence as S
 
 type Point = (Int, Int)
 data Tile = Empty | Wall | Block | Paddle | Ball deriving (Show, Eq)
@@ -19,29 +21,86 @@ intToTile n = case n of
                 4 -> Ball
                 _ -> error "This Int is not associated with a tile"
 
+charTile :: Tile -> Char
+charTile t = case t of
+                 Empty  -> '·'
+                 Wall   -> '▓'
+                 Block  -> '▒'
+                 Paddle -> '─'
+                 Ball   -> '○' 
+
 buildBoard :: Buffer -> Board
-buildBoard (x:y:t:bs) = M.insert (x,y) (intToTile t) (buildBoard bs)
-buildBoard _          = M.empty
+-- buildBoard [] = M.empty
+buildBoard (y:x:t:bs)  
+  | y == -1   = buildBoard bs
+  | otherwise = M.insert (x,y) (intToTile t) (buildBoard bs)
+buildBoard _ = M.empty
+
+scoreBoard :: Buffer -> Int
+scoreBoard [] = 0
+scoreBoard (y:x:score:bs)
+  | y == -1   = score
+  | otherwise = scoreBoard bs
+
+
+boardWidth :: Board -> Int
+boardWidth board = maximum [ snd p | p <- M.keys board ] + 1
+
+displayBoard :: Board -> String
+displayBoard board = unlines $
+                     map (map (charTile . snd)) $
+                     chunksOf (boardWidth board) $
+                     M.toAscList $ 
+                     fixBoard board
+
+-- A hack: for some reason, after stepping the machine, my board is missing the
+-- first few tiles. Let's try a hack!
+
+fixBoard :: Board -> Board
+fixBoard = M.union (M.fromList [((0,0), Wall), ((0,1), Wall), ((0,2), Wall)])
+
+                  
 
 answer1 :: Memory -> Int
 answer1 mem = M.size $ M.filter (== Block) board
-  where buff = buffer $ execute (Machine mem 0 [] 0)
+  where buff = buffer $ execute (Machine mem 1 [] 0)
         board = buildBoard buff
                  
+
+inputToBuffer :: Machine -> Buffer -> Machine
+inputToBuffer (Machine m p _ r) b = Machine m p b r
+
+flushBuffer :: Machine -> Machine
+flushBuffer (Machine m p _ r) = Machine m p [] r
  
+disp = putStrLn . displayBoard . buildBoard . buffer
+
 thirteen :: IO ()
 thirteen = do mem <- getInput "./input"
-              print $ answer1 mem
+              let machFreePlay  = Machine (S.update 0 2 mem) 0 [1] 0
+              let nextmach = execute machFreePlay 
+              let nnextmach = execute $ inputToBuffer nextmach [1]
+              disp nextmach
+              disp nnextmach
 
--- The software draws tiles to the screen with output instructions: every three
--- output instructions specify the x position (distance from the left), y position
--- (distance from the top), and tile id. The tile id is interpreted as follows:
---
-    -- 0 is an empty tile. No game object appears in this tile.  
-    -- 1 is a wall tile.  Walls are indestructible barriers.  
-    -- 2 is a block tile. Blocks can be broken by the ball.  
-    -- 3 is a horizontal paddle tile. The paddle is indestructible.
-    -- 4 is a ball tile. The ball moves diagonally and bounces off objects.
-    --
-    -- How many block tiles are on the screen when the game exits?
 
+-- The game didn't run because you didn't put in any quarters.  Unfortunately,
+-- you did not bring any quarters. Memory address 0 represents the number of
+-- quarters that have been inserted; set it to 2 to play for free.
+
+-- The arcade cabinet has a joystick that can move left and right.  The software
+-- reads the position of the joystick with input instructions:
+
+-- If the joystick is in the neutral position, provide 0.  If the joystick is
+-- tilted to the left, provide -1.  If the joystick is tilted to the right,
+-- provide 1.
+
+-- The arcade cabinet also has a segment display capable of showing a single
+-- number that represents the player's current score. When three output
+-- instructions specify X=-1, Y=0, the third output instruction is not a tile;
+-- the value instead specifies the new score to show in the segment display. For
+-- example, a sequence of output values like -1,0,12345 would show 12345 as the
+-- player's current score.
+
+-- Beat the game by breaking all the blocks. What is your score after the last
+-- block is broken?
